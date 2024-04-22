Fixed rotation issues

routines.py
@@ -10,7 +10,7 @@ def stack_static(to_computer: Queue, from_executor: Queue, stack_positions: list
     static_observation = euler_to_se3(-np.pi, 0, 0, np.array([0.5, -0.15, 0.5]))
     observation_poses = [
         euler_to_se3(-np.pi, -np.pi / 8, 0, np.array([0.45, -0.18, 0.5])),
-        euler_to_se3(-np.pi, np.pi / 8, 0, np.array([0.55, -0.18, 0.5])),
+        euler_to_se3(-np.pi, np.pi / 8, 0, np.array([0.5, -0.18, 0.5])),
         euler_to_se3(-np.pi - np.pi/8, 0, 0, np.array([0.5, -0.13, 0.5])),
         euler_to_se3(-np.pi + np.pi/8, 0, 0, np.array([0.5, -0.22, 0.5])),
     ]
@@ -29,7 +29,6 @@ def stack_static(to_computer: Queue, from_executor: Queue, stack_positions: list
                     sleep(0.1)
                     break
         observed_blocks = from_executor.get()
-        print(observed_blocks)
         for name in observed_blocks:
             block = observed_blocks[name]
             if name not in blocks:
@@ -49,20 +48,39 @@ def stack_static(to_computer: Queue, from_executor: Queue, stack_positions: list
             static_block_poses.append(poses[np.argmin(distances)])
 
 
-    print(static_block_poses)
-
     for i in range(len(static_block_poses)):
-        print(static_block_poses[i].shape)
         rot = static_block_poses[i][:3, :3]
         loc = static_block_poses[i][:3, 3]
-        yaw = np.arctan2(rot[1, 0], rot[0, 0])
-        # find closest 90 degree rotation
-        while yaw < -np.pi / 2:
-            yaw += np.pi / 2
+        forward = np.array([1, 0, 0])
+        up = np.array([0, 0, 1])
+        left = np.array([0, 1, 0])
+        forward_rotated = rot @ forward.T
+        up_rotated = rot @ up.T
+        left_rotated = rot @ left.T
+
+        vectors = [forward_rotated, up_rotated, left_rotated]
+        yaw = None
+        for vector in vectors:
+            angle = np.arccos(np.dot(vector, up))
+            if np.abs(np.abs(angle) - np.pi / 2) < 0.1:
+                yaw = np.arctan2(vector[1], vector[0])
+                break
+
+        if yaw is None:
+            print("Catastrophic error, failed to determine yaw")
+            return
+
         while yaw > np.pi / 2:
             yaw -= np.pi / 2
+        while yaw < -np.pi / 2:
+            yaw += np.pi / 2
+        if yaw > np.pi / 4:
+            yaw -= np.pi / 2
+        if yaw < -np.pi / 4:
+            yaw += np.pi / 2
 
-        print("----------------------Rotation Z", yaw)
+        print("----------------Yaw", np.rad2deg(yaw), "-----------------")
+
 
         loc[2] = 0.225
         static_block_poses[i] = euler_to_se3(-np.pi, 0, yaw, loc)