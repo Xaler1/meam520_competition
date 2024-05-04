3c3ea9642d681b48b92e2a5c1f40f15a5aa47917
Alexander Radchenko <alex.and.radchenko@gmail.com>
2024-05-04 12:36:11 -0400

    added separate calibration script

--- labs/final/calibration.py
+++ labs/final/calibration.py
@@ -118,9 +118,4 @@ if __name__ == "__main__":
     task = Task("open", TaskTypes.BYPASS, command=command)
     main_to_computer.put(task)
 
-    dyn_h = stack_dynamic(main_to_computer, executor_to_main, computer_to_main, DYNAMIC_STACK[:4], config)
-    stat_h = stack_static(main_to_computer, executor_to_main, STATIC_STACK[:4], config)
-    shuffle_blocks(main_to_computer, DYNAMIC_STACK[:dyn_h], STATIC_STACK[stat_h:stat_h + dyn_h])
-    task = Task("other_dynamic", TaskTypes.MOVE_TO, STATIC_STACK[stat_h+dyn_h+3])
-    main_to_computer.put(task)
-    dynamic_grabbed = stack_dynamic(main_to_computer, executor_to_main, computer_to_main, STATIC_STACK[stat_h+dyn_h:stat_h+8], config, timeout=120)
+    calibration(main_to_computer, executor_to_main, config)
